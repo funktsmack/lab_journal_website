aa3cfa0436b30528bb0cc883a255f0ffd52eb7ec
funktsmack <44585605+funktsmack@users.noreply.github.com>
2020-07-03 21:18:08 +0200

    Adding Data Gathering Code to site

---
+++ sales_analysis.R
@@ -0,0 +1,48 @@
+# datascience at NIT ------------------------------------------------------
+# SALES ANALYSIS ----
+
+# 1.0 Load libraries ----
+library(tidyverse)
+#`  library(tibble)    –> is a modern re-imagining of the data frame
+#`  library(readr)     –> provides a fast and friendly way to read rectangular data like csv
+#`  library(dplyr)     –> provides a grammar of data manipulation
+#`  library(magrittr)  –> offers a set of operators which make your code more readable (pipe operator)
+#`  library(tidyr)     –> provides a set of functions that help you get to tidy data
+#`  library(stringr)   –> provides a cohesive set of functions designed to make working with strings as easy as possible
+#`  library(ggplot2)   –> graphics
+
+# 2.0 Importing Files ----
+
+
+# 3.0 Examining Data ----
+
+
+# 4.0 Joining Data ----
+
+
+# 5.0 Wrangling Data ----
+
+
+# 6.0 Business Insights ----
+# 6.1 Sales by Year ----
+
+# Step 1 - Manipulate
+
+# Step 2 - Visualize
+
+
+# 6.2 Sales by Year and Category 2 ----
+
+# Step 1 - Manipulate
+
+# Step 2 - Visualize
+
+
+
+# 7.0 Writing Files ----
+
+# 7.1 Excel ----
+
+# 7.2 CSV ----
+
+# 7.3 RDS ----
